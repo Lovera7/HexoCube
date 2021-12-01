041985d481a757b1f021ee53c1f03bac946a491f
Lovera7 <88889852+Lovera7@users.noreply.github.com>
2021-12-01 09:16:43 -0400

    Agregamos la bala invertida

---
+++ Assets/Bala.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float velocidad = 3;
+
+    void Update()
+    {
+        transform.Translate(0, velocidad * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if (collision.tag == "Asteroide")
+        //{
+        //    collision.GetComponent<Asteroide>().RecibirDanho();
+        //    Destroy(gameObject);
+        //}
+    }
+}
---
+++ Assets/DisparoPersonaje.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoPersonaje : MonoBehaviour
+{
+    public GameObject balaPrototipo;
+    public Transform spawn;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(balaPrototipo, spawn.position, spawn.rotation);
+        }
+    }
+}
