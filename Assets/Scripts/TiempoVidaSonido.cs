beba850115e8f4f21c9230d7eeb53b2e5e813fa3
Steven Gabriel Claros Tapia <StevenClarosT@outlook.com>
2021-12-06 22:07:16 -0400

    sfd

--- Assets/Scripts/TiempoVidaSonido.cs
+++ Assets/Scripts/TiempoVidaSonido.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TiempoVidaSonido : MonoBehaviour
 {
-    public float tiempoVida;
+    public float tiempoVida = 5f;
     // Start is called before the first frame update
     void Start()
     {
